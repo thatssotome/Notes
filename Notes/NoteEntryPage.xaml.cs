326136b45451a7464c2f3c7466997d5cb3c4e10a
Tieumi Nguyen <tieuminguyen@yahoo.com>
2020-05-19 20:52:10 -0400

    Made Styling changes to the app

--- Notes/NoteEntryPage.xaml.cs
+++ Notes/NoteEntryPage.xaml.cs
@@ -14,15 +14,15 @@ namespace Notes
         }
         async void OnSaveButtonClicked(object sender, EventArgs e) //function of the Save button event handler of the NoteEntry Page 
         {
-            var note = (Note)BindingContext;
-            note.Date = DateTime.UtcNow;
+            var note = (Note)BindingContext; //Define the var note files in the app to the (Note)BindingContext of the Database 
+            note.Date = DateTime.UtcNow; //Define the Date of the note files in the app to the DateTime in the Database 
             await App.Database.SaveNoteAsync(note); //previous IF/ELSE statement, now correlates to the IF/ELSE statement in the NoteDatabase class in Data folder
             await Navigation.PopAsync(); //method naviagates back to the previous page 
         }
         async void OnDeleteButtonClicked(object sender, EventArgs e)
         {
             var note = (Note)BindingContext;
-            await App.Database.DeleteNoteAsync(note);
+            await App.Database.DeleteNoteAsync(note); //delete notes in app and database and sync 
             await Navigation.PopAsync();
            
         }
